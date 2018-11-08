03dfe572cf33ca5ecefc304c4b747ec99f8a34eb
Rasch <cxr0002@dcccd.edu>
2018-11-08 13:48:04 -0600

    introduced scaler types in graphql

--- graphql_basics/src/index.js
+++ graphql_basics/src/index.js
@@ -1,38 +1,77 @@
 import { GraphQLServer } from 'graphql-yoga'
 
+// 5 Scaler Types (Scaler means it can store a single value)
+// String, Boolean, Int, Float, ID
+
 // Type definitions (Schema)
 const typeDefs = `
     type Query {
-        hello: String!
+        id: ID!
         name: String!
-        location: String!
-        bio: String!
+        age: Int!
+        isEmployed: Boolean!   
+        gpa: Float
     }
 `
-
+//'!' at the end of our schema queries mean we always want that 'type' back and not 'null'
 
 // Resolvers (Set of functions that are run when various operations are performed)
 const resolvers = {
     Query: {
-        hello() {
-            return 'This is my first query!'
+        id() {
+            return 'abc123'
         },
         name() {
-            return 'Joe Somebody'
+            return 'Chris'
         },
-        location() {
-            return 'Dallas,TX'
+        age() {
+            return 35
         },
-        bio() {
-            return 'I am a Professor of Web Development'
+        isEmployed() {
+            return true
+        },
+        gpa() {
+            return 3.5
+        }
+    }
+}
+
+
+
+const eProduct = `
+    type Query {
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!
+    }
+`
+
+const eProductResolvers = {
+    Query: {
+        title() {
+            return 'iPhone'
+        },
+        price() {
+            return 749.99
+        },
+        releaseYear() {
+            return 2007
+        },
+        rating() {
+            return 4.89
+        },
+        inStock() {
+            return true
         }
     }
 }
 
 //Here, we create our server with a new instance of GraphQLServer and provide the properies above
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
+    typeDefs: eProduct,
+    resolvers: eProductResolvers
 })
 
 //Automatically starts server on port 4000
