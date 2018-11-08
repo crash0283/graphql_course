89ae4e7516afe9fb0ad3d979d08187e2f2e3fdf6
Rasch <cxr0002@dcccd.edu>
2018-11-08 15:11:53 -0600

    added custom types

--- graphql_basics/src/index.js
+++ graphql_basics/src/index.js
@@ -1,77 +1,51 @@
 import { GraphQLServer } from 'graphql-yoga'
 
-// 5 Scaler Types (Scaler means it can store a single value)
-// String, Boolean, Int, Float, ID
-
-// Type definitions (Schema)
 const typeDefs = `
     type Query {
+        me: User!
+        post: Post!
+    }
+
+    type User {
         id: ID!
         name: String!
-        age: Int!
-        isEmployed: Boolean!   
-        gpa: Float
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
     }
 `
-//'!' at the end of our schema queries mean we always want that 'type' back and not 'null'
 
-// Resolvers (Set of functions that are run when various operations are performed)
 const resolvers = {
     Query: {
-        id() {
-            return 'abc123'
+        me() {
+            return {
+                id: '123098',
+                name: 'Mike',
+                email: 'mike@example.com',
+                age: 28
+            }
         },
-        name() {
-            return 'Chris'
-        },
-        age() {
-            return 35
-        },
-        isEmployed() {
-            return true
-        },
-        gpa() {
-            return 3.5
-        }
-    }
-}
-
-
-
-const eProduct = `
-    type Query {
-        title: String!
-        price: Float!
-        releaseYear: Int
-        rating: Float
-        inStock: Boolean!
-    }
-`
-
-const eProductResolvers = {
-    Query: {
-        title() {
-            return 'iPhone'
-        },
-        price() {
-            return 749.99
-        },
-        releaseYear() {
-            return 2007
-        },
-        rating() {
-            return 4.89
-        },
-        inStock() {
-            return true
+        post() {
+            return {
+                id: '39sdkd83jf8438nf8i4kd9',
+                title: 'GraphQL is cool!',
+                body: "I think I'm going to start using GraphQL now!",
+                published: false
+            }
         }
     }
 }
 
 //Here, we create our server with a new instance of GraphQLServer and provide the properies above
 const server = new GraphQLServer({
-    typeDefs: eProduct,
-    resolvers: eProductResolvers
+    typeDefs,
+    resolvers
 })
 
 //Automatically starts server on port 4000
