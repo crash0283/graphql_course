436fac511fbb5983d3e327d759a6ddbf89226a19
rasch <rasch83@gmail.com>
2018-11-08 07:31:30 -0600

    graphql basics start; added babelrc to be able to use import/export

---
+++ graphql_basics/src/index.js
@@ -0,0 +1,5 @@
+import myCurrentLocation,{message,name} from './myModule'
+
+console.log(message)
+console.log(name)
+console.log(myCurrentLocation)
---
+++ graphql_basics/src/myModule.js
@@ -0,0 +1,10 @@
+//named exports - has a name, have as many as needed
+//default export - has no name, can only have one
+
+const message = 'Hello, GraphQL!'
+
+const name = 'Chris'
+
+const location = 'Dallas'
+
+export {message,name,location as default}
