c8a2a2c212e46ab26440e4a86f15b7584a1e8385
Rasch <cxr0002@dcccd.edu>
2018-11-08 13:24:01 -0600

    intro to creating a graphql api

--- graphql_basics/src/index.js
+++ graphql_basics/src/index.js
@@ -1,6 +1,41 @@
-import myCurrentLocation,{message,name,getGreeting} from './myModule'
+import { GraphQLServer } from 'graphql-yoga'
 
-console.log(message)
-console.log(name)
-console.log(myCurrentLocation)
-console.log(getGreeting('Chris'))
+// Type definitions (Schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
+
+
+// Resolvers (Set of functions that are run when various operations are performed)
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query!'
+        },
+        name() {
+            return 'Joe Somebody'
+        },
+        location() {
+            return 'Dallas,TX'
+        },
+        bio() {
+            return 'I am a Professor of Web Development'
+        }
+    }
+}
+
+//Here, we create our server with a new instance of GraphQLServer and provide the properies above
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+//Automatically starts server on port 4000
+server.start(() => {
+    console.log('The server is up!')
+})
